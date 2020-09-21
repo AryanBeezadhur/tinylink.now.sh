1ec5c20313a09f54d9cec0e9400498d7c312cc39
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-09-21 21:11:40 +0100

    Updated UI of api/index.js (lambda)

--- api/index.js
+++ api/index.js
@@ -17,40 +17,59 @@ module.exports = (req, res) => {
 	const linksArray = links.split(',')
 
 	res.write(`
+		<!DOCTYPE html>
+		<html lang="en">
 
-		<style>
-			@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+		<head>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-			* {
-				padding: 0;
-				margin: 0;
-				font-family: 'Inter', sans-serif;
-				font-size: 2.5vh;
-			}
+			<!-- Browser tab -->
+			<title>TinyLink.now.sh|Shorten multiple links into one short URL</title>
+			<link rel="shortcut icon" type="image/x-icon" href="assets/icon.svg" />
 
-			li {
-				margin: 3vh;
-				list-style: none;
-			}
+			<!-- SEO -->
+			<meta name="description" content="Shorten multiple links into one short URL" />
+			<meta name="title" content="TinyLink.now.sh" />
+			<meta name="robots" content="index, follow" />
 
-			li:before {
-				content: '->';
-				color: steelblue;
-			}
+			<!-- OpenGraph -->
+			<meta property="og:title" content="TinyLink.now.sh" />
+			<meta property="og:image" content="https://tinylink.now.sh/assets/icon.svg" />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content="https://tinylink.now.sh" />
+			<meta property="og:site_name" content="tinylink.now.sh" />
+			<meta property="og:description" content="Shorten multiple links into one short URL" />
 
-			a {
-				color: dodgerblue;
-				text-decoration: none;
-			}
+			<!-- Twitter -->
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@AryanBeezadhur" />
+			<meta name="twitter:creator" content="@AryanBeezadhur" />
+			<meta name="twitter:domain" content="tinylink.now.sh" />
+			<meta name="twitter:title" content="TinyLink.now.sh" />
+			<meta name="twitter:description" content="Shorten multiple links into one short URL" />
+			<meta name="twitter:image" content="https://tinylink.now.sh/assets/icon.svg" />
+			<meta name="twitter:image:alt" content="Shorten multiple links into one short URL" />
+			<meta name="twitter:url" content="https://tinylink.now.sh" />
 
-			a:hover {
-				text-decoration: underline;
-			}
-		</style>
+			<!-- Stylesheet -->
+			<link rel="stylesheet" href="style.css">
 
+		</head>
+
+		<body>
+
+			<div id="container">
+
+				<h1 class="api-page-element">
+					<img src="assets/icon.svg" alt="Logo">
+					TinyLink.now.sh
+				</h1>
+
+				<h2 class="api-page-element">Saved links</h2>
+
+				<ul>
 	`)
-
-	res.write(`<ul>`)
 
 	for (var linksArrayIndex = 0; linksArrayIndex < linksArray.length; linksArrayIndex++) {
 		res.write(`
@@ -62,7 +81,24 @@ module.exports = (req, res) => {
 		`)
 	}
 
-	res.write(`</ul>`)
+	res.write(`
+				</ul>
+
+				<footer class="api-page-element">
+					<p>
+						<a href="https://github.com/AryanBeezadhur/tinylink.now.sh">Open-source</a>
+						project created by
+						<a href="https://aryanbeezadhur.com">Aryan Beezadhur</a>
+					</p>
+				</footer>
+			</div>
+	`)
+
+	res.write(`
+		</body>
+
+		</html>
+	`)
 
 	res.end()
 }
