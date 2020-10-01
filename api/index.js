4a6631bcf63f3e697b95af755ac453c73cfeb99c
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-10-01 11:46:23 +0100

    Removed Google Analytics, added Chaport

--- api/index.js
+++ api/index.js
@@ -37,16 +37,6 @@ module.exports = (req, res) => {
 
 		<head>
 
-			<!-- Global site tag (gtag.js) - Google Analytics -->
-			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179354718-2"></script>
-			<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-			
-			gtag('config', 'UA-179354718-2');
-			</script>
-
 			<meta charset="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -119,6 +109,19 @@ module.exports = (req, res) => {
 	`)
 
 	res.write(`
+
+		<!-- Begin of Chaport Live Chat code -->
+		<script type="text/javascript">
+			(function (w, d, v3) {
+				w.chaportConfig = {
+					appId: '5f7481125b7324048fa4cd8b'
+				};
+
+				if (w.chaport) return; v3 = w.chaport = {}; v3._q = []; v3._l = {}; v3.q = function () { v3._q.push(arguments) }; v3.on = function (e, fn) { if (!v3._l[e]) v3._l[e] = []; v3._l[e].push(fn) }; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://app.chaport.com/javascripts/insert.js'; var ss = d.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss)
+			})(window, document);
+		</script>
+		<!-- End of Chaport Live Chat code -->
+
 		</body>
 
 		</html>
