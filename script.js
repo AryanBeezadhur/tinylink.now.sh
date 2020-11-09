531c6f853d72b3fe688e8eba91cd800ca89f8e8d
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-11-09 21:08:52 +0000

    Revert "Format code with prettier"
    This reverts commit b41ad57bcb9d220771bfd2c61877a903eda27650.

--- script.js
+++ script.js
@@ -1,78 +1,86 @@
-const $form = document.getElementById("form");
-const $addURL = document.getElementById("add-url");
-const $urlInputGroup = document.getElementById("url-input-group");
-const $submitBtn = document.getElementById("submit-btn");
-var $urlInputFields = [...document.querySelectorAll(".url-input")];
+const $form = document.getElementById('form')
+const $addURL = document.getElementById('add-url')
+const $urlInputGroup = document.getElementById('url-input-group')
+const $submitBtn = document.getElementById('submit-btn')
+var $urlInputFields = [...document.querySelectorAll('.url-input')]
 
 var updateURLInputFields = () => {
-  $urlInputFields = [...document.querySelectorAll(".url-input")];
-};
+    $urlInputFields = [...document.querySelectorAll('.url-input')]
+}
 
 var copyText = (textToCopy) => {
-  const tempTextarea = document.createElement("textarea");
-  tempTextarea.value = textToCopy;
-  document.body.appendChild(tempTextarea);
-  tempTextarea.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempTextarea);
-};
+    const tempTextarea = document.createElement('textarea')
+    tempTextarea.value = textToCopy
+    document.body.appendChild(tempTextarea)
+    tempTextarea.select()
+    document.execCommand('copy')
+    document.body.removeChild(tempTextarea)
+}
 
-$form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  updateURLInputFields();
+$form.addEventListener('submit', (evt) => {
 
-  for (var i = 0; i < $urlInputFields.length; i++) {
-    if ($urlInputFields[i].value.includes("#") == true) {
-      $urlInputFields[i].value = $urlInputFields[i].value.split("#")[0];
+    evt.preventDefault()
+    updateURLInputFields()
+
+    for (var i = 0; i < $urlInputFields.length; i++) {
+
+        if ($urlInputFields[i].value.includes('#') == true) {
+            $urlInputFields[i].value = $urlInputFields[i].value.split('#')[0]
+        }
+
     }
-  }
-});
+})
 
-$addURL.addEventListener("click", () => {
-  const newURLInput = document.createElement("input");
-  newURLInput.classList = "url-input";
-  newURLInput.type = "url";
-  newURLInput.placeholder = "https://tinylink.now.sh";
-  $urlInputGroup.appendChild(newURLInput);
+$addURL.addEventListener('click', () => {
 
-  const newLineBreak = document.createElement("br");
-  $urlInputGroup.appendChild(newLineBreak);
+    const newURLInput = document.createElement('input')
+    newURLInput.classList = 'url-input'
+    newURLInput.type = 'url'
+    newURLInput.placeholder = 'https://tinylink.now.sh'
+    $urlInputGroup.appendChild(newURLInput)
 
-  updateURLInputFields();
-  $submitBtn.innerText = `Shorten ${$urlInputFields.length} links`;
-});
+    const newLineBreak = document.createElement('br')
+    $urlInputGroup.appendChild(newLineBreak)
 
-$submitBtn.addEventListener("click", async () => {
-  var urlToShorten;
-  updateURLInputFields();
+    updateURLInputFields()
+    $submitBtn.innerText = `Shorten ${$urlInputFields.length} links`
 
-  for (var i = 0; i < $urlInputFields.length; i++) {
-    if ($urlInputFields[i].value != "") {
-      urlToShorten += `${$urlInputFields[i].value},`;
+})
+
+$submitBtn.addEventListener('click', async () => {
+
+    var urlToShorten
+    updateURLInputFields()
+
+    for (var i = 0; i < $urlInputFields.length; i++) {
+
+        if ($urlInputFields[i].value != '') {
+            urlToShorten += `${$urlInputFields[i].value},`
+        }
+
     }
-  }
 
-  // Abort if urlToShorten is still undefined
-  if (urlToShorten == undefined) {
-    return alert("Please enter at least 1 link");
-  }
+    // Abort if urlToShorten is still undefined
+    if (urlToShorten == undefined) {
+        return alert('Please enter at least 1 link')
+    }
 
-  // Add Tinylink API URL, remove trailing comma, remove 'undefined' from start of string if present
-  urlToShorten = `https://tinylink.now.sh/api?links=${urlToShorten}`;
-  urlToShorten = urlToShorten.replace(/,\s*$/, "");
-  if (urlToShorten.includes("undefined") == true) {
-    urlToShorten = urlToShorten.replace("undefined", "");
-  }
+    // Add Tinylink API URL, remove trailing comma, remove 'undefined' from start of string if present
+    urlToShorten = `https://tinylink.now.sh/api?links=${  urlToShorten}`
+    urlToShorten = urlToShorten.replace(/,\s*$/, "")
+    if (urlToShorten.includes('undefined') == true) {
+        urlToShorten = urlToShorten.replace('undefined', '')
+    }
 
-  try {
-    var res = await fetch(
-      `https://tinyurl.com/api-create.php?url=${urlToShorten}`
-    );
-    var TinyURL = await res.text();
-    $submitBtn.style = "height: 60px";
-    $submitBtn.innerHTML = `${TinyURL} <br /><br /> - Link copied to clipboard!`;
-    copyText(TinyURL);
-  } catch (error) {
-    alert(error);
-  }
-});
+    try {
+
+        var res = await fetch(`https://tinyurl.com/api-create.php?url=${urlToShorten}`)
+        var TinyURL = await res.text()
+        $submitBtn.style = 'height: 60px'
+        $submitBtn.innerHTML = `${TinyURL} <br /><br /> - Link copied to clipboard!`
+        copyText(TinyURL)
+
+    } catch (error) {
+        alert(error)
+    }
+})
