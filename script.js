e247fb263d11c5926d4de40f78011deab2b2e64e
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-11-10 09:32:47 +0000

    Removed redundant padding in blocks

--- script.js
+++ script.js
@@ -18,21 +18,17 @@ var copyText = (textToCopy) => {
 }
 
 $form.addEventListener('submit', (evt) => {
-
     evt.preventDefault()
     updateURLInputFields()
 
     for (var i = 0; i < $urlInputFields.length; i++) {
-
         if ($urlInputFields[i].value.includes('#') == true) {
             $urlInputFields[i].value = $urlInputFields[i].value.split('#')[0]
         }
-
     }
 })
 
 $addURL.addEventListener('click', () => {
-
     const newURLInput = document.createElement('input')
     newURLInput.classList = 'url-input'
     newURLInput.type = 'url'
@@ -44,20 +40,16 @@ $addURL.addEventListener('click', () => {
 
     updateURLInputFields()
     $submitBtn.innerText = `Shorten ${$urlInputFields.length} links`
-
 })
 
 $submitBtn.addEventListener('click', async () => {
-
     var urlToShorten
     updateURLInputFields()
 
     for (var i = 0; i < $urlInputFields.length; i++) {
-
         if ($urlInputFields[i].value != '') {
             urlToShorten += `${$urlInputFields[i].value},`
         }
-
     }
 
     // Abort if urlToShorten is still undefined
@@ -66,20 +58,18 @@ $submitBtn.addEventListener('click', async () => {
     }
 
     // Add Tinylink API URL, remove trailing comma, remove 'undefined' from start of string if present
-    urlToShorten = `https://tinylink.now.sh/api?links=${  urlToShorten}`
+    urlToShorten = `https://tinylink.now.sh/api?links=${urlToShorten}`
     urlToShorten = urlToShorten.replace(/,\s*$/, "")
     if (urlToShorten.includes('undefined') == true) {
         urlToShorten = urlToShorten.replace('undefined', '')
     }
 
     try {
-
         var res = await fetch(`https://tinyurl.com/api-create.php?url=${urlToShorten}`)
         var TinyURL = await res.text()
         $submitBtn.style = 'height: 60px'
         $submitBtn.innerHTML = `${TinyURL} <br /><br /> - Link copied to clipboard!`
         copyText(TinyURL)
-
     } catch (error) {
         alert(error)
     }
